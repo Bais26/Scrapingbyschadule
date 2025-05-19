4e095e31d53b9d6f1416f051a8dbd33003e92b14
Bais Yufan <baisyufan2004@gmail.com>
2025-05-19 21:38:13 +0700

    first commit

---
+++ scraper/scheduler.py
@@ -0,0 +1,13 @@
+import schedule
+import time
+from scraper.article_scraper import scrape_and_store_articles
+
+def run_scheduler():
+    # Jadwalkan scraping setiap hari pukul 08:00
+    schedule.every().day.at("21:29").do(scrape_and_store_articles)
+
+    print("📅 Scheduler aktif. Menunggu waktu eksekusi...")
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
